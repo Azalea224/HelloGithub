53e53e4a2e9879237ab93af00e120e5f82f788e7
azalea224 <azalea224@users.noreply.github.com>
2025-10-06 17:35:55 +0300

    chore: sync

--- main.ts
+++ main.ts
@@ -3,8 +3,15 @@ console.log("Hello, world!");
 console.log("this is a test");
 //Meow
 let name = "Meow";
+//Variables declared with let have block scope, meaning they are accessible only within the block (e.g., if statement, for loop, function) where they are defined. 
+//They can be reassigned to a new value.
 var age = 24;
+//Variables declared with var have function scope, meaning they are accessible within the function where they are defined.
+//They can be reassigned to a new value.
 const isStudent = true;
+//Variables declared with const have block scope, meaning they are accessible only within the block (e.g., if statement, for loop, function) where they are defined.
+//They cannot be reassigned to a new value.
 if (isStudent) {console.log("You are a student");}
 else {console.log("You are not a student");}
+//if statements checks if the condition is true, if it is, it will execute the code inside the if statement, if it is false, it will execute the code inside the else statement.
 //variables and if statements
