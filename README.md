f9cc96adfda66347d75bd15ae38b0e64ac83336f
azalea224 <azalea224@users.noreply.github.com>
2025-10-06 17:17:04 +0300

    variables and if statements

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 This is for CODED Academy
 meow
+![alt text](Katsu_Curry_7011bsq.jpg)
--- main.ts
+++ main.ts
@@ -2,3 +2,9 @@
 console.log("Hello, world!");
 console.log("this is a test");
 //Meow
+let name = "Meow";
+var age = 24;
+const isStudent = true;
+if (isStudent) {console.log("You are a student");}
+else {console.log("You are not a student");}
+//variables and if statements
